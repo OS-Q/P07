34563ba99e25ddd77b5196fee78db14dfa2e7f97
qitas <qitas@qitas.cn>
2021-05-04 09:51:50 +0800

    init

---
+++ README.md
@@ -0,0 +1,15 @@
+# [P07](https://github.com/OS-Q/P07)
+
+[![sites](http://182.61.61.133/link/resources/OSQ.png)](http://www.OS-Q.com)
+
+[![Build Status](https://github.com/OS-Q/P07/workflows/CI/badge.svg)](https://github.com/OS-Q/P07/actions/workflows/CI.yml)
+[![Build Status](https://circleci.com/gh/OS-Q/P07.svg?style=svg)](https://circleci.com/gh/OS-Q/P07)
+[![Build Status](https://cloud.drone.io/api/badges/OS-Q/P07/status.svg)](https://cloud.drone.io/OS-Q/P07)
+
+### [简介](https://github.com/OS-Q/P07/wiki)
+
+
+[P07](https://github.com/OS-Q/P07)
+
+
+### [源圈 OS-Q (Open Source & QA) ](http://www.OS-Q.com)
---
+++ platform.py
@@ -0,0 +1,118 @@
+import os
+import platform
+
+from platformio.managers.platform import PlatformBase
+
+
+class P07Platform(PlatformBase):
+
+    def get_boards(self, id_=None):
+        result = PlatformBase.get_boards(self, id_)
+        if not result:
+            return result
+        if id_:
+            return self._add_default_debug_tools(result)
+        else:
+            for key, value in result.items():
+                result[key] = self._add_default_debug_tools(result[key])
+        return result
+
+    def _add_default_debug_tools(self, board):
+        debug = board.manifest.get("debug", {})
+        if "tools" not in debug:
+            debug["tools"] = {}
+
+        tools = (
+            "digilent-hs1",
+            "digilent-hs2",
+            "olimex-arm-usb-tiny-h",
+            "olimex-arm-usb-ocd-h",
+            "olimex-arm-usb-ocd",
+            "olimex-jtag-tiny",
+            "ovpsim",
+            "renode"
+        )
+        for tool in tools:
+            if tool in debug["tools"]:
+                continue
+
+            if tool == "ovpsim":
+                debug["tools"][tool] = {
+                    "init_cmds": [
+                        "define pio_reset_halt_target",
+                        "end",
+                        "define pio_reset_run_target",
+                        "end",
+                        "set mem inaccessible-by-default off",
+                        "set arch riscv:rv32",
+                        "set remotetimeout 250",
+                        "target extended-remote $DEBUG_PORT",
+                        "$INIT_BREAK",
+                        "$LOAD_CMDS",
+                    ],
+                    "server": {
+                        "package": "tool-ovpsim-corev",
+                        "arguments": [
+                            "--variant", "CV32E40P",
+                            "--port", "3333",
+                            "--program",
+                            "$PROG_PATH"
+                        ],
+                        "executable": "riscvOVPsimCOREV"
+                    },
+                    "onboard": True
+                }
+
+            elif tool == "renode":
+                debug["tools"][tool] = {
+                    "server": {
+                        "package": "tool-renode",
+                        "arguments": [
+                            "--disable-xwt",
+                            "-e", "include @%s" % os.path.join(
+                                self.get_dir(), "misc", "renode", "openhw_cv32e40p.resc"),
+                            "-e", "machine StartGdbServer 3333 True"
+                        ],
+                        "ready_pattern": "GDB server with all CPUs started on port",
+                        "executable": ("bin/Renode.exe" if platform.system() == "Windows" else "renode")
+                    },
+                    "onboard": True
+                }
+
+            else:
+                debug["tools"][tool] = {
+                    "init_cmds": [
+                        "define pio_reset_halt_target",
+                        "   load",
+                        "   monitor reset halt",
+                        "end",
+                        "define pio_reset_run_target",
+                        "   load",
+                        "   monitor reset",
+                        "end",
+                        "set mem inaccessible-by-default off",
+                        "set arch riscv:rv32",
+                        "set remotetimeout 250",
+                        "target extended-remote $DEBUG_PORT",
+                        "$INIT_BREAK",
+                        "$LOAD_CMDS",
+                    ],
+                    "server": {
+                        "package": "tool-openocd-riscv-pulp",
+                        "executable": "bin/openocd",
+                        "arguments": [
+                            "-s",
+                            os.path.join(self.get_dir(), "misc", "openocd"),
+                            "-s",
+                            "$PACKAGE_DIR/share/openocd/scripts",
+                            "-f",
+                            ("digilent-hs2.cfg" if tool == "digilent-hs2" else ("interface/ftdi/%s.cfg" % tool)),
+                            "-f",
+                            "cv32e40p_nexys.cfg",
+                        ]
+                    },
+                    "onboard": tool in debug.get("onboard_tools", []),
+                }
+
+        board.manifest["debug"] = debug
+        return board
